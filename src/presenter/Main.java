e73f346ade93692332f240fa0ac9192b5257f629
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2023-02-25 15:45:50 -0500

    Linea de codigo añadida

--- src/presenter/Main.java
+++ src/presenter/Main.java
@@ -2,6 +2,7 @@ package presenter;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Holaaaaaa");
         System.out.println("Hello world!");
     }
 }
