dc0d16438e02ccf5bb0f3dd1b76216fd50ca4ca3
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2023-02-26 23:18:36 -0500

    Arreglo

--- src/presenter/Main.java
+++ src/presenter/Main.java
@@ -2,10 +2,9 @@ package presenter;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Sapo X3");
-=======
+
         System.out.println(" sapo x2");
->>>>>>> Logica
+
     }
 }
